88d8b590ee86b83048219a8e8d3175bb71ce0134
Oliver Tongrak <tongrak_ru@cmu.ac.th>
2021-01-08 16:02:07 +0700

    Lab09_3 done!!!

--- lab9_3.cpp
+++ lab9_3.cpp
@@ -1,3 +1,23 @@
-//cout << "Number of data = ";
-//cout << "Mean = ";
-//cout << "Standard deviation = ";
+#include<iostream>
+#include<iomanip>
+#include<cmath>
+#include<fstream>
+using namespace std;
+
+int main(){
+    int N=0;
+    double x=0,y=0;
+    string si;
+    ifstream ScorFile("score.txt");
+    while(getline(ScorFile, si)){
+        x += stod(si);
+        y += pow(stod(si),2);
+        N++;
+    }
+    ScorFile.close();
+    double mean = x/N, sd = sqrt((y/N)-pow(mean,2));
+    cout <<"Number of data = "<<N;
+    cout <<setprecision(3)<<"\nMean = "<<mean<<"\nStandard deviation = "<<sd;
+
+    return 0;
+}
